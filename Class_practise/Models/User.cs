c6727414bfacccdd94a4083805caea4e8f16a8c9
User <elxan.memmedli2003@gmail.com>
2023-11-16 03:21:38 +0400

    First commit

---
+++ Class_practise/Models/User.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_practise.Models
+{
+    internal class User
+    {
+        private string _userName;
+        private string _password;
+        private bool _isSignedIn = false;
+        private Product[] Products = new Product[0];
+
+        public string UserName 
+        {
+            get
+            {
+                return _userName;
+            }
+            set
+            {
+                if (value.Trim().Length >= 6 && value.Trim().Length <= 25 ) 
+                {
+                    _userName = value;
+                }
+            } 
+        }
+        public string Password 
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                if (value.Trim().Length >= 8 && value.Trim().Length <= 25 && CheckPassword(value) == true)
+                {
+                    _password = value;
+                }
+                else
+                {
+                    Console.WriteLine("nagaysan ay qaa"); 
+                }
+            } 
+        }
+        public bool IsSignedIn 
+        {
+            get
+            {
+                return _isSignedIn;
+            }
+            set
+            {
+                _isSignedIn = value;
+            }
+        }
+
+        public User(string username,string password)
+        {
+            UserName = username;
+            Password = password;
+        }
+
+        public bool CheckPassword(string text)
+        {
+            if (HasDigit(ref text) == true && HasUpper(text) == true && HasLower(text) == true) 
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool HasDigit(ref string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsDigit(text[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool HasLower(string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsLower(text[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool HasUpper(string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsUpper(text[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Login(string username,string password)
+        {
+            if (username == UserName && password == Password)
+            {
+                IsSignedIn = true;
+                return;
+            }
+            IsSignedIn = false;
+        }
+        public void Add(Product product)
+        {
+            startLabel:
+            if (IsSignedIn == true)
+            {
+                Array.Resize(ref Products, Products.Length + 1);
+                Products[Products.Length - 1] = product;
+                Console.WriteLine("Mehsul elave edildi");
+                return;
+            }
+            else
+            {
+                bool condition = true;
+                while (condition == true) 
+                {
+                    Console.WriteLine("Siz hesabiniza daxil olmamisiniz zehmet olmasa teleb olunan parametrleri daxil edin");
+                    Console.Write("Adi daxil edin : ");
+                    string username1 = Console.ReadLine();
+                    Console.Write("Parolu daxil edin : ");
+                    string password1 = Console.ReadLine();
+                    if (username1 == UserName && password1 == Password)
+                    {
+                        Login(username1, password1);
+                        Console.WriteLine("Hesaba ugurla daxil olundu");
+                        goto startLabel;
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Class_practise/Program.cs
@@ -0,0 +1,30 @@
+﻿using Class_practise.Models;
+
+namespace Class_practise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*User yaradirsiz:
+            Username(6,25),Password(8,25), IsSignedIn, Products propertyleri olur
+
+            Product yaradirsiz:
+            Name, Price propertyleri olur
+
+            CheckPassword methodu olur geriye bool qaytarir kicik boyuk herfleri,ve reqem olub olmadigini yoxluyur.
+            Login methodu olur istifadeci deyerleri ekrandan daxil edir.
+            Add methodu olur eger istifadeci login olubsa Products arrayina productu elave edir.
+            Eger olmuyubsa bir basha Login methodunu cagirir*/
+
+            User user = new User("Brazen","Yenisifre1");
+            Product product = new Product("Limon",0.50f);
+            Product product1 = new Product("Dondurma",1f);
+            Product product2 = new Product("Cips",3f);
+            Product product3 = new Product("Cola",1.2f);
+            //user.Login("Brazen","Yenisifre1");
+            user.Add(product);
+        }
+       
+    }
+}
